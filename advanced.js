2aace9b1268076b0e1d11647305b048c3200f328
hamza benlafkih <hamzabenlafkih90@gmail.com>
2021-08-30 09:51:06 +0100

    the power of 'this'

--- advanced.js
+++ advanced.js
@@ -577,3 +577,15 @@ const healArcher = wizard.heal.bind(archer);
 console.log(archer);
 healArcher();
 console.log(archer);
+
+// -- function currying
+
+function multiply(a, b) {
+  return a * b;
+}
+
+var multipleByTwo = multiply.bind(this, 2);
+console.log(multipleByTwo(4));
+
+var multipleByThree = multiply.bind(this, 3);
+console.log(multipleByThree(4));
