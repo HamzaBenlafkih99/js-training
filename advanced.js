6b6fe72de1c425b76adee56de2011bf04a0b5548
hamza benlafkih <hamzabenlafkih90@gmail.com>
2021-09-01 16:10:00 +0100

    example 2

--- advanced.js
+++ advanced.js
@@ -1100,3 +1100,36 @@ and if he doesn't find anything, he start to looking up of the prototype chaine 
 object because we're just override the default prototype object with dragon
 
 */
+
+// example 2
+let dragon = {
+  name: "Tanya",
+  fire: true,
+  fight() {
+    return 5;
+  },
+  sing() {
+    if (this.fire) {
+      return `I am ${this.name}, the breather of fire`;
+    }
+  },
+};
+
+let lizard = {
+  name: "Kiki",
+  fight() {
+    return 1;
+  },
+};
+
+lizard.__proto__ = dragon;
+for (let prop in lizard) {
+  console.log(prop);
+}
+
+/* output :
+name
+fight
+fire
+sing
+*/
