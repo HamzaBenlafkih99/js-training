14ec086cd56ba327b9354d10f0df9f1a8714358c
hamza benlafkih <hamzabenlafkih90@gmail.com>
2021-09-02 16:59:14 +0100

    ahahaha

--- advanced.js
+++ advanced.js
@@ -1311,4 +1311,58 @@ const peter = new Elf("Peter", "bow");
 console.log(same.name);
 console.log(same.atack());
 
-console.log(sam instanceof Elf);
+console.log(sam instanceof Elf); // true
+
+/*
+does javascript has class ?
+yes it have but just in a syntatic seggure which mean class keyword at the end of the day is just 
+prototyple inheretance, and some peaple called it --> sudo classes inheretance
+*/
+
+// --- this keyword
+// --> new binding this
+
+// new binding
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  console.log(this);
+}
+
+const person1 = new Person("Xavier", 55);
+
+//implicit binding
+const person = {
+  name: "Karen",
+  age: 40,
+  hi() {
+    console.log("hi" + this.name);
+  },
+};
+
+person.hi();
+
+//explicit binding
+const person3 = {
+  name: "Karen",
+  age: 40,
+  hi: function () {
+    console.log("hi" + this.setTimeout);
+  }.bind(window),
+};
+
+person3.hi();
+
+// arrow functions
+const person4 = {
+  name: "Karen",
+  age: 40,
+  hi: function () {
+    var inner = () => {
+      console.log("hi " + this.name);
+    };
+    return inner();
+  },
+};
+
+person4.hi();
