07901d897b742702106ac2bc388f569eac2efdba
hamza benlafkih <hamzabenlafkih90@gmail.com>
2021-09-02 20:39:31 +0100

    class inheritance

--- advanced.js
+++ advanced.js
@@ -1366,3 +1366,40 @@ const person4 = {
 };
 
 person4.hi();
+
+// Class inheritance
+
+class Character {
+  constructor(name, weapon) {
+    this.name = name;
+    this.weapon = weapon;
+  }
+  attack() {
+    return "atack with " + this.weapon;
+  }
+}
+
+class Elf extends Character {
+  constructor(name, weapon, type) {
+    // console.log('what am i?', this); this gives an error
+    super(name, weapon);
+    console.log("what am i?", this);
+    this.type = type;
+  }
+}
+
+class Ogre extends Character {
+  constructor(name, weapon, color) {
+    super(name, weapon);
+    this.color = color;
+  }
+  makeFort() {
+    // this is like extending our prototype.
+    return "strongest fort in the world made";
+  }
+}
+
+const houseElf = new Elf("Dolby", "cloth", "house");
+//houseElf.makeFort() // error
+const shrek = new Ogre("Shrek", "club", "green");
+shrek.makeFort();
