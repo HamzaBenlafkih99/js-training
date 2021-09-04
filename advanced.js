70767c2590a7ef6cbb2c11ad653020e36f8c4089
hamza benlafkih <hamzabenlafkih90@gmail.com>
2021-09-04 09:41:46 +0100

    solution exercice

--- advanced.js
+++ advanced.js
@@ -1652,4 +1652,64 @@ essencialy the same thing exept instead of right to left it goes left to right
 
 const Pipe = (f, g) => (data) => g(f(data)); // the opposite of compose :)
 
-// Arity --> number of argument a function take
+// Arity --> number of arguments a function take
+
+// Amazon shopping
+const user = {
+  name: "Kim",
+  active: true,
+  cart: [],
+  purchases: [],
+};
+//Implement a cart feature:
+// 1. Add items to cart.
+// 2. Add 3% tax to item in cart
+// 3. Buy item: cart --> purchases
+// 4. Empty cart
+
+//Bonus:
+// accept refunds.
+// Track user history.
+
+purchaseItem(
+  emptyCart,
+  buyItem,
+  applyTaxToItems,
+  addItemToCart
+)(user, { name: "laptop", price: 200 });
+
+const compose =
+  (f, g) =>
+  (...args) =>
+    f(g(...args));
+
+function purchaseItem(...fns) {
+  return fns.reduce(compose);
+}
+
+function addItemToCart(user, item) {
+  const updatedCart = user.cart.concat(item);
+  return Object.assign({}, user, { cart: updatedCart });
+}
+
+function applyTaxToItems(user) {
+  const { cart } = user;
+  const taxRate = 1.3;
+  const updatedCart = cart.map((item) => {
+    return {
+      item: item.name,
+      price: item.price * taxRate,
+    };
+  });
+  return Object.assign({}, user, { cart: updatedCart });
+}
+
+function buyItem(user) {
+  return Object.assign({}, user, { purchases: user.cart });
+}
+
+function emptyCart(user) {
+  return Object.assign({}, user, { cart: [] });
+}
+
+function refundItem() {}
